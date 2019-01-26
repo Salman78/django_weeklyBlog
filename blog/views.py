b3d186a8791e427bf3f9b8de12d2c3634053eac9
Tausal21 <salmanrefayet@gmail.com>
2019-01-25 20:30:10 -0500

    TESTING WITH DUMMY DATA (POSTS)
    Inside blog apps views.py file, we create two dummy posts using dictionary and
    store them in a context dictionary and pass it to render in the template. We
    then edit home.html by adding some code-block to loop over the posts to show
    in the frontend. We haven't used any databases yet.

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+posts = [
+    {
+        'author': 'salman',
+        'title': 'Blog Post 1',
+        'content': 'First Post Content',
+        'date posted': 'january 25, 2019'
+    },
+    {
+        'author': 'jon',
+        'title': 'Blog Post 2',
+        'content': 'Second Post Content',
+        'date posted': 'january 26, 2019'
+    }
+]
+
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
-# Create your views here.
+
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
+
+def about(request):
+    return render(request, 'blog/about.html')
