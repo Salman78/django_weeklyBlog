2448df1439b54a63613fc8de0f1bf7d983f4dee3
Tausal21 <salmanrefayet@gmail.com>
2019-01-29 02:47:26 -0500

    CREATED USERS APP, SET UP VIEWS.PY FOR FORM AND NEW TEMPLATE REGISTER.HTML

--- blog/views.py
+++ blog/views.py
@@ -2,20 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Post
 
-posts = [
-    {
-        'author': 'salman',
-        'title': 'Blog Post 1',
-        'content': 'First Post Content',
-        'date posted': 'january 25, 2019'
-    },
-    {
-        'author': 'jon',
-        'title': 'Blog Post 2',
-        'content': 'Second Post Content',
-        'date posted': 'january 26, 2019'
-    }
-]
 
 def home(request):
 
