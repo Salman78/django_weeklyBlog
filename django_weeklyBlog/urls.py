85cbc0e4dd438207af636db03412ae082a8efa49
Tausal21 <salmanrefayet@gmail.com>
2019-01-30 03:26:12 -0500

    model based login, logout view imported, url route set up and login template created

--- django_weeklyBlog/urls.py
+++ django_weeklyBlog/urls.py
@@ -2,9 +2,12 @@
 from django.contrib import admin
 from django.urls import path, include
 from users import views as user_views
+from django.contrib.auth import views as auth_views #model based view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
     path('register/', user_views.register, name='register'),
+    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),   #creates backend logic but not template for frontend
+    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
 ]
