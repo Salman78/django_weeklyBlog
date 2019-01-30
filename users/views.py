c00457607718054378e297bfa65b71e1f9e82d51
Tausal21 <salmanrefayet@gmail.com>
2019-01-29 23:20:41 -0500

    SEPARATE FORM FILE CREATED, INHERITED FROM USERCREATIONFORM AND ADDED EMAIL FIELD

--- users/views.py
+++ users/views.py
@@ -1,16 +1,17 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
+from .forms import UserRegisterForm
 
 def register(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST) #request.POST is a dictionary containing info sent via POST request
+        form = UserRegisterForm(request.POST) #request.POST is a dictionary containing info sent via POST request
         #we want this request.POST dictionary to specifically contain info regarding user form input
         if form.is_valid():
+            form.save()
             username = form.cleaned_data.get('username')
             messages.success(request, f'Account created for {username}!')
             return redirect('blog-home')
     else:
-        form = UserCreationForm()
+        form = UserRegisterForm()
 
     return render(request, 'users/register.html', {'form': form})
