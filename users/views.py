e8ec0535a1ffcd07cd0636b7f5486572bc35c135
Tausal21 <salmanrefayet@gmail.com>
2019-01-29 22:29:27 -0500

    FORM INPUT VALIDATED AND USER REDIRECT IN VIWES.PY

--- users/views.py
+++ users/views.py
@@ -1,6 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+
 def register(request):
-    form = UserCreationForm()
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST) #request.POST is a dictionary containing info sent via POST request
+        #we want this request.POST dictionary to specifically contain info regarding user form input
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account created for {username}!')
+            return redirect('blog-home')
+    else:
+        form = UserCreationForm()
 
     return render(request, 'users/register.html', {'form': form})
