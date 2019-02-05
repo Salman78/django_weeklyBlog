47ab205ea80699c6afe24af44015b5d9c4cd057a
Tausal21 <salmanrefayet@gmail.com>
2019-02-05 17:16:49 -0500

    UPDATE FORMS CREATED AND RENDERED
    UserUpdateForm and ProfileUpdateForm created inside the users app's forms.py module. both rendered together in the profile.html template so in the frontend they appear in a single page and looks like a single form. enctype tag added on form tag. Also u_form and p_form object created inside views.py file

--- users/forms.py
+++ users/forms.py
@@ -1,11 +1,26 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from .models import Profile #to modify profile picture
 
 class UserRegisterForm(UserCreationForm):
- email = forms.EmailField()
+    email = forms.EmailField()
 
- class Meta:
-     model = User
-     fields = ['username', 'email', 'password1', 'password2']
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
+
+#forms to update a specific model, in this case, will update user model
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['username', 'email']
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['image']   #we have two different forms but when we put in this together inside a templage, it will look like one single form in the frontend
+
 
