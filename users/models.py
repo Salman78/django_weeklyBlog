5814188e47043388f268182b387508521102fdad
Tausal21 <salmanrefayet@gmail.com>
2019-02-24 23:28:12 -0500

    Image resizing enabled by overriding save() method in user's models.py

--- users/models.py
+++ users/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from PIL import Image
+
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -7,3 +9,16 @@ class Profile(models.Model):
 
     def __str__(self):
         return f'{self.user.username} Profile'
+
+    #overriding parent class's save method to size image after resizing
+    #There are many ways of resizing image
+    def save(self):
+        super().save()
+
+        #opening image for this profile instance
+        img = Image.open(self.image.path)
+
+        if img.height > 300 or img.width > 300:
+            output_size = (300, 300)
+            img.thumbnail(output_size)
+            img.save(self.image.path) #passing the same path to override the image
